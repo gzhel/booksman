4c782a212d78caf5defe4a48b818a2247aac3411
gregory <gregoryzhel@gmail.com>
2021-05-11 16:01:04 +0300

    0.3.2: BM - market page init v2

---
+++ src/pages/market/store/model.js
@@ -0,0 +1,12 @@
+export const marketInitial = {
+    booksList: [],
+    bookInfo: [],
+    bookComments: [],
+    showBuyModal: false,
+    showBuySuccessModal: false,
+    showBuyFailedModal: false,
+};
+
+export const marketActionTypes = {
+    GET_BOOKS_LIST: 'MARKET.GET_BOOKS_LIST',
+};
